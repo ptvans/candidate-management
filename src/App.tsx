a27b2fc773b38fb23ed4e396439fdb7a770a53bb
sven <lappy@MacBook-Pro-3.local>
2025-08-07 14:12:49 -0700

    use hasrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import CandidateList from './pages/CandidateList';
 import CandidateDetail from './pages/CandidateDetail';
